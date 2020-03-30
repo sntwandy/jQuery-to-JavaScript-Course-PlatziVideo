1b3274db7564c6107b4a8cb89acc88d67fd1a076
sntwandy <sntwandy@gmail.com>
2020-03-30 04:57:43 -0400

    Practicing about Promise

--- src/js/home.js
+++ src/js/home.js
@@ -6,3 +6,33 @@ let change = "@sntwandy"
 function changeName(newName) {
   change = newName
 }
+
+// Promises
+
+const getUsers = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    resolve('Everything is OK')
+    //reject('Not OK')
+  }, 3000)
+})
+
+const getAllUsers = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    resolve('Everything is OK')
+    //reject('Not OK')
+  }, 5000)
+})
+
+// Execute all promises (array) and returns a array with success ir not success.
+Promise.all([getUsers, getAllUsers]).then(function(message){
+  console.log(message)
+}).catch(function(message){
+  console.log(message)
+})
+
+// Execute the first promise that execute with suceess or not success.
+/*Promise.race([getUsers, getAllUsers]).then(function(message){
+  console.log(message)
+}).catch(function(message){
+  console.log(message)
+})*/
